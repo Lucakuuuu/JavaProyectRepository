1deb13ef60664cfd785719425a1cd935d51817ca
Lucakuuuu <163463371+Lucakuuuu@users.noreply.github.com>
2024-08-28 22:09:56 -0400

    avances en las funciones principales

--- ProyectoJava/src/tarea/Asignatura.java
+++ ProyectoJava/src/tarea/Asignatura.java
@@ -4,24 +4,24 @@ package tarea;
 public class Asignatura
   {
     private String nombre;
-    private int algo;
+    private int codigo;
     private int nota;
     
-    public Asignatura(String nombre, int algo, int nota)
+    public Asignatura(String nombre, int codigo)
     {
       this.nombre = nombre;
-      this.algo = algo;
-      this.nota = nota;
+      this.codigo = codigo;
+      this.nota = 0;
     }
 
-    public void setNombre(String nombre)
+	public void setNombre(String nombre)
     {
       this.nombre = nombre;
     }
     
-    public void setAlgo(int algo)
+    public void setCodigo(int codigo)
     {
-      this.algo = algo;
+      this.codigo = codigo;
     }
 
     public void setNota(int nota)
@@ -34,13 +34,19 @@ public class Asignatura
       return nombre;
     }
 
-    int getAlgo()
+    int getCodigo()
     {
-      return algo;
+      return codigo;
     }
 
     int getNota()
     {
       return nota;
+    }
+    
+    public void mostrarAsignatura() {
+    	System.out.println(" ---------- " + nombre + " ----------\n"
+    			+ "Código: " + codigo + "\n"
+    					+ "Nota: " + nota + " \n---------- ");
     }
   }
--- ProyectoJava/src/tarea/Main.java
+++ ProyectoJava/src/tarea/Main.java
@@ -1,65 +1,65 @@
 package tarea;
-import java.util.Scanner;
-/*
-registro de bancos de preguntas por temas 
-instancias de evaluación a partir de los bancos 
-registro de notas de cada evaluación.
-
-Lo que falta (avance):
-  -Se debe hacer un menú para el Sistema donde ofrezca las funcionalidades de: 1) Inserción
-   Manual / agregar elemento.
-*/
+import java.util.*;
 
 public class Main 
 {
-  // print(HOLA, BIENEIDO AL SISTEMA DE EVALUACION POR ASINATURA, ingresa tu nombre);
   public static void main(String[] args) 
   {
     Scanner entrada = new Scanner(System.in);
     int opcion = 0;
     boolean salir = false;
-    opcion = entrada.nextInt();
+
+    ArrayList<Asignatura> lista = new ArrayList<>();
+
+    for(int i = 0; i < 3; i++) {
+    	System.out.println("Ingrese la asignatura: ");
+    	String nombre = entrada.nextLine();
+    	Asignatura asignatura = new Asignatura(nombre, i+1);
+    	lista.add(asignatura);
+    }
     
-    System.out.println("| ---------- | Elija una opcion | ---------- |");
+    System.out.println("| ---------- | Elija una opción | ---------- |");
 
-    while(!salir)
-      {
-        System.out.println("1) INICIAR EVALUACIÓN\n2) REGISTRO DE NOTAS\n3) PRÁCTICA\n4) SOLUCIONARIO\n5) Salir");
-        System.out.println("Ingrese una opción: ");
-        opcion = entrada.nextInt();
+    while(!salir) 
+    {
+      System.out.println("1) INICIAR EVALUACIÓN\n2) REGISTRO DE NOTAS\n3) PRÁCTICA\n4) SOLUCIONARIO\n5) Salir");
+      System.out.println("Ingrese una opción: ");
+      opcion = entrada.nextInt();
 
-        switch(opcion)
-          {
-            case 1:
-              System.out.println("| ---------- | Iniciar evaluación | ---------- |");
-              tarea.LimpiarPantalla.limpiarPantalla();
-              break;
+      switch(opcion) {
+        case 1:
+          System.out.println("| ---------- | Iniciar evaluación | ---------- |");
+          tarea.LimpiarPantalla.limpiarPantalla();
+          break;
 
-            case 2:
-              System.out.println("| ---------- | Registro de notas | ---------- | ");
-              tarea.LimpiarPantalla.limpiarPantalla();
-              break;
-
-            case 3:
-              System.out.println("| ---------- | Práctica | ---------- | ");
-              tarea.LimpiarPantalla.limpiarPantalla();
-              break;
-
-            case 4:
-              System.out.println("| ---------- | Solucionarío | ---------- | ");
-              tarea.LimpiarPantalla.limpiarPantalla();
-              break;
-
-            case 5:
-              System.out.println("Saliendo del programa...");
-              salir = true;
-              tarea.LimpiarPantalla.limpiarPantalla();
-              break;
-
-            default:
-              System.out.println("Ingrese una opción valida");
+        case 2:
+          System.out.println("| ---------- | Registro de notas | ---------- | ");
+          for(int j = 0; j < lista.size(); j++) {
+        	  lista.get(j).mostrarAsignatura();
           }
+          tarea.LimpiarPantalla.limpiarPantalla();
+          break;
+
+        case 3:
+          System.out.println("| ---------- | Práctica | ---------- | ");
+          tarea.LimpiarPantalla.limpiarPantalla();
+          break;
+
+        case 4:
+          System.out.println("| ---------- | Solucionarío | ---------- | ");
+          tarea.LimpiarPantalla.limpiarPantalla();
+          break;
+
+        case 5:
+          System.out.println("Saliendo del programa...");
+          salir = true;
+          tarea.LimpiarPantalla.limpiarPantalla();
+          break;
+
+        default:
+          System.out.println("Ingrese una opción valida");
       }
+    }
     entrada.close();
   }  
 }
